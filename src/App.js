ddf05a225a34115b1a6b10c7dda862fd1f1dd450
Dilipraj10 <dilipraj5655@gmail.com>
2024-04-23 19:40:23 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+// import RegisterPage from './Components/RegisterPage';
+import Routing from './Components/Routing';
+// import ForgotPassword from './Components/ForgotPassword';
+// import AdminSignIn from './Components/AdminSignIn';
+// import SignIn from './Components/SignIn';
+
+function App() {
+  return (
+    <div>
+      {/* <SignIn/> */}
+      {/* <AdminSignIn/> */}
+      {/* <RegisterPage/> */}
+      {/* <ForgotPassword/> */}
+      <Routing/>
+    </div>
+  );
+}
+
+export default App;
